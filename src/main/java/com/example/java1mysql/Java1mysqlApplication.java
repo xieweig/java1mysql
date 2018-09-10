9cb9a5f4e7d6f63560ea0bde3cf2a0dd3c5d7dd6
xieweig <3162217932@qq.com>
2018-09-10 10:22:31 +0800

    Initial commit

---
+++ src/main/java/com/example/java1mysql/Java1mysqlApplication.java
@@ -0,0 +1,12 @@
+package com.example.java1mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java1mysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java1mysqlApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/java1mysql/Java1mysqlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.java1mysql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Java1mysqlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
